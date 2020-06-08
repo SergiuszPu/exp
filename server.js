579c8a55ad0a9f6d1478e9b82119f3f1734edfab
Sergiusz Pu <serson85@gmail.com>
2020-06-08 11:01:08 +0200

    add Project manage

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const path = require('path');
 const hbs = require('express-handlebars');
 
 const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+
 // app.engine('hbs', hbs());
 app.set('view engine', 'hbs');
 app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './views', defaultLayout: 'main' }));
@@ -16,17 +19,35 @@ app.get('/', (req, res) => {
 app.get('/hello/:name', (req, res) => {
   res.render('hello', { layout: false, name: req.params.name });
 });
+
 app.get('/dark.css', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/dark.css'));
-  });
+});
+
 app.get('/about', (req, res) => {
   res.render('about');
 });
 
+// app.post('/contact/send-message', (req, res) => {
+//   res.json(req.body);
+// });
+
+app.post('/contact/send-message', (req, res) => {
+
+  const { author, sender, title, message, file} = req.body;
+
+  if(author && sender && title && message && file) {
+    res.render('contact', { isSent: true, file});
+  }
+  else {
+    res.render('contact', { isError: true });
+  }
+
+});
 
 app.get('/dark', (req, res) => {
     res.render('dark');
-  });
+});
 
 app.get('/contact', (req, res) => {
   res.render('contact');
